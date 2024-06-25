d69319978dcf36dc835225004e2d637fda312eee
DruMoDev <dev.morera@gmail.com>
2024-06-25 11:07:26 +0200

    whitelist

--- index.js
+++ index.js
@@ -18,12 +18,19 @@ if (process.env.NODE_ENV === "production") {
 conectarDB();
 
 // Configurar CORS
-let whitelist = ["https://task-planner-dru.vercel.app", "https://task-planner-backend.vercel.app", "http://localhost:5173/", "http://localhost:5173"];
-// let whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL];
+let whitelist = [
+  "https://task-planner-dru.vercel.app",
+  "https://task-planner-backend.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5173/",
+];
 
+// let whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL];
 
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log("Origin:", origin); // Agrega esta línea para depuración
+
     if (whitelist.includes(origin)) {
       // Puede consultar la API
       callback(null, true);
